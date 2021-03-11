bba2e30a0150d33b1b46e2cc5cc5e54fb95d70c8
mwkho <martin.h@hotmail.ca>
2021-03-11 21:43:33 +0000

    completed the breed fetcher function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,20 @@
-let request = require('request');
+const request = require('request');
 
+const breed = process.argv[2];
+const URL = 'https://api.thecatapi.com/v1/breeds/search?name=' + breed;
+
+
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log("There was an error on the page\n Error Message:\n", error);
+    return;
+  }
+  const data =  JSON.parse(body);
+  const firstResult = data[0];
+  if (!firstResult) {
+    console.log("You have inputted an invalid cat breed.");
+    return;
+  }
+  console.log(firstResult.description);
+});

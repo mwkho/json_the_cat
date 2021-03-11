247a79b95e97f860ecee17b4076acd449212b4ac
mwkho <martin.h@hotmail.ca>
2021-03-11 22:08:46 +0000

    putt he request code into a function expresion

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,25 @@
 const request = require('request');
 
 const breed = process.argv[2];
-const URL = 'https://api.thecatapi.com/v1/breeds/search?name=' + breed;
+
+const fetchBreedDescription = (breedName, callback) => {
+  const URL = 'https://api.thecatapi.com/v1/breeds/search?name=' + breedName;
+  request(URL, (error, response, body) => {
+    if (error) {
+      console.log("There was an error on the page\n Error Message:\n", error);
+      return;
+    }
+    const data =  JSON.parse(body);
+    const firstResult = data[0];
+    if (!firstResult) {
+      console.log("You have inputted an invalid cat breed.");
+      return;
+    }
+    console.log(firstResult.description);
+  });
+ }
 
 
-
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log("There was an error on the page\n Error Message:\n", error);
-    return;
-  }
-  const data =  JSON.parse(body);
-  const firstResult = data[0];
-  if (!firstResult) {
-    console.log("You have inputted an invalid cat breed.");
-    return;
-  }
-  console.log(firstResult.description);
-});
+ module.exports ={
+   fetchBreedDescription
+ }
